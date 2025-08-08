2b73e9418c734fcfde5164c856e0c384b9bed8ba
NyiNyiZin-cmd <nyinyizin951@gmail.com>
2025-08-08 17:16:19 +0900

    collection map method

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -25,22 +25,17 @@ class Blog
 
     public static function all()
     {
-        $files = File::files(resource_path("blogs"));
-        return array_map(function ($file) {
-            $obj = YamlFrontMatter::parseFile($file);
-            return  new Blog($obj->title, $obj->slug, $obj->intro, $obj->body());
-        }, $files);
+
+        return collect(File::files(resource_path("blogs")))
+            ->map(function ($file) {
+                $obj = YamlFrontMatter::parseFile($file);
+                return  new Blog($obj->title, $obj->slug, $obj->intro, $obj->body());
+            });
     }
 
     public static function find($slug)
     {
-        $path = resource_path("blogs/$slug.html");
-        if (!file_exists($path)) {
-            return redirect('/');
-        }
-
-        return cache()->remember("posts.$slug", now()->addSeconds(120), function () use ($path) {
-            return   file_get_contents($path);
-        });
+       $blogs=static::all();
+       return $blogs->firstWhere('slug',$slug);
     }
 }
--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -10,9 +10,9 @@
 </head>
 
 <body>
-    <?= $blog ?>
+    <h1> <?= $blog->title ?></h1>
+    <p> <?= $blog->body ?></p>
     <a href="/">go back</a>
-
 </body>
 
 </html>
