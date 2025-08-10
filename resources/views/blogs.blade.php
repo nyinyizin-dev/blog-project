c22cd905b044bbb8db4ba60b9349a1af92355108
NyiNyiZin-cmd <nyinyizin951@gmail.com>
2025-08-10 17:00:25 +0900

    design upgrade with blade components

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,21 +1,5 @@
 <x-layout>
-    <x-slot name="title">
-        <title>All Blogs</title>
-    </x-slot>
-    @foreach ($blogs as $blog)
-        <div>
-            <h1>
-                <a href="blogs/{{ $blog->slug }}">
-                    {{ $blog->title }}
-                </a>
-            </h1>
-            <p>Author - <a href="/users/{{ $blog->author->username }}">{{ $blog->author->name }}</a></p>
-            <p>Category - <a href="/categories/{{ $blog->category->slug }}">{{ $blog->category->name }}</a></p>
-
-            <div>
-                <p>published at - {{ $blog->created_at->diffForHumans() }}</p>
-                <p>{{ $blog->intro }}</p>
-            </div>
-        </div>
-    @endforeach
+    <x-hero />
+    <x-blog-section />
+    <x-scribe />
 </x-layout>
