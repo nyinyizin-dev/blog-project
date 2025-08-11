d4d5e6071d85c10d193a8aef330141557d3ca0ac
NyiNyiZin-cmd <nyinyizin951@gmail.com>
2025-08-12 00:09:13 +0900

    adding filter method by searching word from title and body

--- resources/views/components/blog-section.blade.php
+++ resources/views/components/blog-section.blade.php
@@ -1,4 +1,4 @@
-@props(['blogs','categories','currentCategory'])
+@props(['blogs', 'categories', 'currentCategory'])
 
 <section class="container text-center" id="blogs">
     <h1 class="display-5 fw-bold mb-4">Blogs</h1>
@@ -6,14 +6,14 @@
         <div class="dropdown">
             <button class="btn btn-outline-primary dropdown-toggle" type="button" id="dropdownMenuButton1"
                 data-bs-toggle="dropdown" aria-expanded="false">
-               {{ isset($currentCategory) ? $currentCategory->name : 'Filter By Category'}}
+                {{ isset($currentCategory) ? $currentCategory->name : 'Filter By Category' }}
             </button>
             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                 @foreach ($categories as $category)
-                     <li><a class="dropdown-item" href="/categories/{{ $category->slug }}">{{ $category->name }}</a></li>
+                    <li><a class="dropdown-item" href="/categories/{{ $category->slug }}">{{ $category->name }}</a></li>
                 @endforeach
-               
-              
+
+
             </ul>
         </div>
         {{-- <select name="" id="" class="p-1 rounded-pill mx-3">
@@ -22,17 +22,22 @@
     </div>
     <form action="" class="my-3">
         <div class="input-group mb-3">
-            <input type="text" autocomplete="false" class="form-control" placeholder="Search Blogs..." />
+            <input name="search" type="text" autocomplete="false" class="form-control" placeholder="Search Blogs..."
+                value="{{ request('search') }}" />
             <button class="input-group-text bg-primary text-light" id="basic-addon2" type="submit">
                 Search
             </button>
         </div>
     </form>
     <div class="row">
-        @foreach ($blogs as $blog)
+
+        @forelse ($blogs as $blog)
             <div class="col-md-4 mb-4">
                 <x-blog-card :blog="$blog" />
             </div>
-        @endforeach
+        @empty
+            <p class="text-center">No Blogs Found.</p>
+        @endforelse
+
     </div>
 </section>
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,20 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Models\Blog;
 use App\Models\Category;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('blogs', [
-        'blogs' => Blog::latest()->get(),
-        'categories' => Category::all()
-    ]);
-});
+Route::get('/', [BlogController::class, 'index']);
 
-Route::get('/blogs/{blog:slug}', function (Blog $blog) {
-    return view('blog', [
-        'blog' => $blog,
-        'randomBlogs' => Blog::inRandomOrder()->take(3)->get()
-    ]);
-})->where('blog', '[A-z\d\-_]+');
+Route::get('/blogs/{blog:slug}', [BlogController::class, 'show']);
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('blogs', [
         'blogs' =>  $category->blogs,
         'categories' => Category::all(),
-        'currentCategory'=> $category
+        'currentCategory' => $category
     ]);
 });
 
