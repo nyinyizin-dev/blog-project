cff519d2bd0bbb7a6ea11ca2a17bb37dcc3ed80b
NyiNyiZin-cmd <nyinyizin951@gmail.com>
2025-08-12 17:52:14 +0900

    adding register form

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,35 @@
+<x-layout>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 mx-auto">
+                <h3 class="text-primary text-center mt-3">Register Form</h3>
+                <div class="card p-4 my-3 shadow-sm">
+                    <form method="POST">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" name="name"
+                                aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">User Name</label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" name="username"
+                                aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" name="email"
+                                aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BlogController::class, 'index']);
 Route::get('/blogs/{blog:slug}', [BlogController::class, 'show']);
 
+Route::get('/register',[AuthController::class, 'create']);
+Route::post('/register',[AuthController::class, 'store']);
