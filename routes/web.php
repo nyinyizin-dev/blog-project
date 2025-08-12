a6174e8ef860185f81f1753bc2f6419a9055381b
NyiNyiZin-cmd <nyinyizin951@gmail.com>
2025-08-12 15:44:57 +0900

    filter with username

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,8 @@
 <?php
 
 use App\Http\Controllers\BlogController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BlogController::class, 'index']);
 Route::get('/blogs/{blog:slug}', [BlogController::class, 'show']);
 
-Route::get('/users/{user:username}', function (User $user) {
-    return view('blogs', [
-        'blogs' =>  $user->blogs,
-    ]);
-});
