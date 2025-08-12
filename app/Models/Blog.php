1e819baa65d56273606826db7afd1a0552ea4347
NyiNyiZin-cmd <nyinyizin951@gmail.com>
2025-08-12 10:09:11 +0900

    logical grouping

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -15,8 +15,10 @@ class Blog extends Model
     public function scopeFilter($query, $filter) // Blog::latest()->filter()
     {
         $query->when($filter['search']??false, function ($query, $search) {
-            $query->where('title', 'LIKE', '%' . $search . '%')
+           $query->where(function ($query) use ($search) {
+             $query->where('title', 'LIKE', '%' . $search . '%')
                 ->orWhere('body', 'LIKE', '%' . $search . '%');
+           });
         });
     }
     public function category()
